2ccc45db9c0a6940ac0f1c97ad2a935940e2a876
andanfed <andanfe@gmail.com>
2020-06-13 02:39:43 +0300

    Add associations for ResetToken and CommentReaction

--- server/src/data/db/associations.js
+++ server/src/data/db/associations.js
@@ -1,9 +1,11 @@
 export default models => {
   const {
     User,
+    ResetToken,
     Post,
     PostReaction,
     Comment,
+    CommentReaction,
     Image
   } = models;
 
@@ -14,17 +16,25 @@ export default models => {
   User.hasMany(Comment);
   User.hasMany(PostReaction);
   User.belongsTo(Image);
+  User.belongsTo(ResetToken);
+
+  ResetToken.hasOne(User);
 
   Post.belongsTo(Image);
   Post.belongsTo(User);
   Post.hasMany(PostReaction);
   Post.hasMany(Comment);
 
+  Comment.hasMany(CommentReaction);
   Comment.belongsTo(User);
   Comment.belongsTo(Post);
 
   PostReaction.belongsTo(Post);
   PostReaction.belongsTo(User);
+
+  CommentReaction.belongsTo(Comment);
+  CommentReaction.belongsTo(Post);
+  CommentReaction.belongsTo(User);
 };
 
 // Add CommentReaction association
