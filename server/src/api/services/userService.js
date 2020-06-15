b52663ffb00d863d21864d25a0eea4b19e3b77e6
andanfed <andanfe@gmail.com>
2020-06-15 17:16:12 +0300

    Add status field for user on fetch

--- server/src/api/services/userService.js
+++ server/src/api/services/userService.js
@@ -1,8 +1,8 @@
 import userRepository from '../../data/repositories/userRepository';
 
 export const getUserById = async userId => {
-  const { id, username, email, imageId, image } = await userRepository.getUserById(userId);
-  return { id, username, email, imageId, image };
+  const { id, username, email, imageId, status, image } = await userRepository.getUserById(userId);
+  return { id, username, email, imageId, status, image };
 };
 
 // updating user via users id => needed to sent new user object at all
